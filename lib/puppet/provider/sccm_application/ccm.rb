9ef638c6e021168bfdd441e228a6cec715494d50
Trevor Howard <thoward@godaddy.com>
2015-04-23 17:00:01 -0700

    prefetch working

--- lib/puppet/provider/sccm_application/ccm.rb
+++ lib/puppet/provider/sccm_application/ccm.rb
@@ -1,60 +1,19 @@
 require 'win32ole' if Puppet.features.microsoft_windows?
 
 class WIN32OLE
-  def select(attr_name, value)
-    selected = Array.new()
+  def get(attr_name, value)
     self.each do |x|
       x_value = x.invoke(attr_name)
       if x_value =~ /^#{value}$/i or x_value == value
-        selected.push(x)
+        return x
       end
     end
-
-    return selected
   end
 end
 
 class CCM
-  def initialize
-    @wmi_session = WIN32OLE.connect("winmgmts://localhost/root/ccm/clientSDK")
-    @application_list = @wmi_session.ExecQuery("select * from CCM_Application")
-  end
-  
-  def evaluate_success(action)
-    return_values = {
-      0 => "success",
-      2 => "access denied",
-      8 => "unknown failure",
-      9 => "invalid name",
-      10 => "invalid level",
-      21 => "invalid parameter",
-      22 => "duplicate share",
-      23 => "redirected path",
-      24 => "unknown directory",
-      25 => "net name not found",
-    }
-  
-    raise(return_values[action]) unless action == 0
-  end
-  
-  def install(name)
-    wmi = WIN32OLE.connect("winmgmts:Win32_Share")
-    print "#{@path}\n#{@name}\n"
-    creation = wmi.create(@path, @name, 0, nil, nil)
-    evaluate_success(creation)
-  end
-  
-  def uninstall(name)
-    deletion = @wmi_session.get("Win32_Share='#{@name}'").delete
-    evaluate_success(deletion)
-  end
-
-  def list
-    @application_list
-  end
-
-  def get(name)
-    self.list.select('name', name)[0]
+  class << self
+    attr_accessor :list
   end
 end
 
@@ -65,12 +24,18 @@ Puppet::Type.type(:sccm_application).provide :windows do
   mk_resource_methods
 
   def self.instances
-	ccm_instance.list.each.collect do |application|
-	  if application.IsMachineTarget and application.InstallState == "Installed"
-        new( :name => application.name,
-             :ensure => :present
-        )
-      end
+    wmi_session = WIN32OLE.connect("winmgmts://localhost/root/ccm/clientSDK")
+	CCM.list = wmi_session.ExecQuery("select * from CCM_Application")
+	CCM.list.each.collect do |application|
+    if application.InstallState == "Installed"
+	  state = :present
+    else
+      state = :absent
+    end
+
+      new( :name => application.name.downcase,
+           :ensure => state
+      )
     end
   end
 
@@ -86,12 +51,6 @@ Puppet::Type.type(:sccm_application).provide :windows do
   def initialize(value={})
     super(value)
     @property_flush = {}
-  end
-
-  def ccm_instance
-    return @ccm_instance if defined?(@ccm_instance)
-	@ccm_instance = CCM.new
-    @ccm_instance
   end
 
   def exists?
--- lib/puppet/type/sccm_application.rb
+++ lib/puppet/type/sccm_application.rb
@@ -14,13 +14,32 @@ Puppet::Type.newtype(:sccm_application) do
   end
 
   newparam(:name) do
-    desc "Name of SCCM package made available in Software Center through deployment."
+    desc "The name of the sccm_application resource. Used for uniqueness. Will set
+      the target to this value if target is unset."
     validate do |value|
       if value.nil? or value.empty?
         raise ArgumentError, "A non-empty name must be specified."
       end
     end
 
+    munge do |value|
+      value = value.downcase
+    end
+
   isnamevar
+  end
+
+  newparam(:target) do
+    desc "The application package name that is advertised through a deployment.
+      The default is the name."
+    validate do |value|
+      if value.nil? or value.empty?
+        raise ArgumentError, "A non-empty target must be specified."
+      end
+    end
+
+    munge do |value|
+      value = value.downcase
+    end
   end
 end
